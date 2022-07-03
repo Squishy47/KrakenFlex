06aa28ac6363ebabee99784b4eaf5dac83894f6e
Sam <stheman50@gmail.com>
2022-07-03 19:26:44 +0100

    covert dataAccess to module

--- src/DAO/DataAccess.ts
+++ src/DAO/DataAccess.ts
@@ -1,51 +1,48 @@
 import fetch from "node-fetch";
-
 import { FailedRequest } from "../Errors/FailedReqest.js";
 import { Outage, IOutageDAO } from "../Entities/Outage.js";
 import { ISiteDao, Site } from "../Entities/Site.js";
 import { Device } from "../Entities/Device.js";
 import { Config } from "../Config/Config.js";
 
-export default class DataAccess {
-	private async makeFetchRequest(route: string, data: any) {
-		const res = await fetch(route, data);
+async function makeFetchRequest(route: string, data: any) {
+	const res = await fetch(route, data);
 
-		if (!res.ok) {
-			throw new FailedRequest(`Error fetching API data ${await res.text()}`);
-		}
-
-		return await res.json();
+	if (!res.ok) {
+		throw new FailedRequest(`Error fetching API data ${await res.text()}`);
 	}
 
-	async getOutages(): Promise<Outage[]> {
-		const data = (await this.makeFetchRequest(await (await Config()).routes.outages, {
-			method: "GET",
-			headers: { "x-api-key": await (await Config()).apiKey },
-		})) as IOutageDAO[];
+	return await res.json();
+}
 
-		return data.map((item) => new Outage(item.id, item.begin, item.end));
-	}
+export async function getOutages(): Promise<Outage[]> {
+	const data = (await makeFetchRequest(await (await Config()).routes.outages, {
+		method: "GET",
+		headers: { "x-api-key": await (await Config()).apiKey },
+	})) as IOutageDAO[];
 
-	async getSiteById(siteId: string) {
-		const data = (await this.makeFetchRequest(`${await (await Config()).routes.siteInfo}/${siteId}`, {
-			method: "GET",
-			headers: { "x-api-key": await (await Config()).apiKey },
-		})) as ISiteDao;
+	return data.map((item) => new Outage(item.id, item.begin, item.end));
+}
 
-		return new Site(
-			data.id,
-			data.name,
-			data.devices.map((dev) => new Device(dev.id, dev.name))
-		);
-	}
+export async function getSiteById(siteId: string) {
+	const data = (await makeFetchRequest(`${await (await Config()).routes.siteInfo}/${siteId}`, {
+		method: "GET",
+		headers: { "x-api-key": await (await Config()).apiKey },
+	})) as ISiteDao;
 
-	async PostFilteredOutagesBySiteId(siteId: string, outages: { id: string; begin: string; end: string; name: string }[]): Promise<boolean> {
-		await this.makeFetchRequest(`${await (await Config()).routes.siteOutages}/${siteId}`, {
-			method: "POST",
-			body: JSON.stringify(outages),
-			headers: { "x-api-key": await (await Config()).apiKey },
-		});
+	return new Site(
+		data.id,
+		data.name,
+		data.devices.map((dev) => new Device(dev.id, dev.name))
+	);
+}
 
-		return true;
-	}
+export async function PostFilteredOutagesBySiteId(siteId: string, outages: { id: string; begin: string; end: string; name: string }[]): Promise<boolean> {
+	await makeFetchRequest(`${await (await Config()).routes.siteOutages}/${siteId}`, {
+		method: "POST",
+		body: JSON.stringify(outages),
+		headers: { "x-api-key": await (await Config()).apiKey },
+	});
+
+	return true;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { Config } from "./Config/Config.js";
-import DataAccess from "./DAO/DataAccess.js";
+import { getOutages, getSiteById, PostFilteredOutagesBySiteId } from "./DAO/DataAccess.js";
 
 const dateToFilterOutagesBy = "2022-01-01T00:00:00.000Z";
 
 main();
 
 async function main() {
-	const dataAccess = new DataAccess();
-
-	const outages = await dataAccess.getOutages();
-	const site = await dataAccess.getSiteById(await (await Config()).siteId);
+	const outages = await getOutages();
+	const site = await getSiteById(await (await Config()).siteId);
 
 	const filteredOutages = outages.filter((outage) => !outage.didStartBeforeDate(new Date(dateToFilterOutagesBy)));
 
@@ -17,7 +15,5 @@ async function main() {
 
 	const listOfFormattedOutages = site.getListOfOutages();
 
-	(await dataAccess.PostFilteredOutagesBySiteId(await (await Config()).siteId, listOfFormattedOutages))
-		? console.log("successfully send data to system")
-		: false;
+	(await PostFilteredOutagesBySiteId(await (await Config()).siteId, listOfFormattedOutages)) ? console.log("successfully send data to system") : false;
 }
