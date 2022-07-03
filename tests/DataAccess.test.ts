cc47fb6f97ef75b9787f44947f13b9481ce1e0c8
Sam <stheman50@gmail.com>
2022-07-03 20:19:03 +0100

    add unit tests for data Access

--- tests/DataAccess.test.ts
+++ tests/DataAccess.test.ts
@@ -1,49 +1,65 @@
-import DataAccess from "../src/DAO/DataAccess.js";
-import { Outage } from "../src/Entities/Outage.js";
-import { FailedRequest } from "../src/Errors/FailedReqest.js";
-import fetch, { Response } from "node-fetch";
+import { IOutageDAO, Outage } from "../src/Entities/Outage.js";
+import { getOutages, getSiteById } from "../src/DAO/DataAccess.js";
+import { Site } from "../src/Entities/Site.js";
+import { Device } from "../src/Entities/Device.js";
+import * as realConfig from "../src/Config/Config.js";
 
-jest.mock("node-fetch", () => ({
-	fetch: jest.fn(),
-}));
+let mockApiData: any | undefined = undefined;
+
+jest.mock(`node-fetch`, () => {
+	const generateResponse = () => ({
+		ok: true,
+		json: (): IOutageDAO[] => mockApiData,
+		text: () => "hello world",
+	});
+
+	return jest.fn().mockResolvedValue(generateResponse());
+});
 
 describe("DataAccess", () => {
-	const dataAccess = new DataAccess();
-
-	beforeEach(() => {
-		// jest.mocked(fetch).mockClear();
+	beforeAll(() => {
+		const mock = jest.spyOn(realConfig, "Config"); // spy on foo
+		mock.mockImplementation(async () => ({
+			baseUrl: "baseUrl",
+			apiKey: "apiKey",
+			siteId: "norwich-pear-tree",
+			routes: {
+				outages: `/outages`,
+				siteInfo: `/site-info`,
+				siteOutages: `/site-outages`,
+			},
+		}));
 	});
 
 	test("getOutages", async () => {
-		jest.mocked(fetch).mockImplementation((): Promise<any> => {
-			return Promise.resolve({
-				json() {
-					return Promise.resolve([
-						{
-							id: "123",
-							begin: "2022-01-02T01:00:00.000Z",
-							end: "2022-01-03T01:01:00.000Z",
-						},
-					]);
-				},
-			});
-		});
-		const data = await dataAccess.getOutages();
-
+		mockApiData = [
+			{
+				id: "123",
+				begin: "2022-01-02T01:00:00.000Z",
+				end: "2022-01-03T01:01:00.000Z",
+			},
+		];
+		const data = await getOutages();
 		const outage = new Outage("123", "2022-01-02T01:00:00.000Z", "2022-01-03T01:01:00.000Z");
-
 		expect(data).toMatchObject([outage]);
 	});
 
-	test("getOutagesError", async () => {
-		const data = await dataAccess.getOutages();
+	test("get site by id", async () => {
+		mockApiData = {
+			id: "norwich-pear-tree",
+			name: "Norwich Pear Tree",
+			devices: [
+				{
+					id: "111183e7-fb90-436b-9951-63392b36bdd2",
+					name: "Battery 1",
+				},
+			],
+		};
 
-		const outage = new Outage("123", "2022-01-02T01:00:00.000Z", "2022-01-03T01:01:00.000Z");
+		const data = await getSiteById("norwich-pear-tree");
 
-		expect(data).toThrowError(new FailedRequest("ERROR"));
+		const site = new Site("norwich-pear-tree", "Norwich Pear Tree", [new Device("111183e7-fb90-436b-9951-63392b36bdd2", "Battery 1")]);
+
+		expect(data).toMatchObject(site);
 	});
-
-	// test("getSiteById", () => {});
-
-	// test("PostFilteredOutagesBySiteId", () => {});
 });
